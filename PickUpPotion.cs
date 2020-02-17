7557891420604f0f192bbe427c40e9800a6d95b6
Miguel S <MFSV_148@hotmail.com>
2020-02-17 14:22:41 -0600

    Pick-Up Script
    Added Script for pickups

---
+++ PickUpPotion.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpPotion : MonoBehaviour {
+    //heals player 25
+    //oil 25
+    [SerializeField] private int potionHealth = 25;
+    [SerializeField] private int potionOil = 25;
+
+    void OnTriggerEnter(Collider other) {
+        //detects pick up
+        if (other.gameObject.CompareTag("Player")) {
+           //destroy the pick up from hierarchy 
+           Destroy(this.gameObject);
+		}
+	}
+}
