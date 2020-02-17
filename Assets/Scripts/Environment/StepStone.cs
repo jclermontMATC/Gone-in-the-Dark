94a4c3cad75c655f95486085635fabbdfd2477ac
Sean Harvey <harveysr@gmatc.matc.edu>
2020-02-17 14:58:47 -0600

    Updated Lighting Configuration
    Updated light source to a spot light
    Added mouse pointing
    Added step stone script & sprite

---
+++ Assets/Scripts/Environment/StepStone.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+// Require that some sort of Collider is attached to the object
+[RequireComponent(typeof(Collider))]
+public class StepStone : MonoBehaviour {
+    // Unity Event provides a simple way to link other objects to the stepstone without explicitly defining the connections in code.
+    public UnityEvent OnStep; // This is public so it can be accessed from other scripts if needed.
+
+    private void OnTriggerEnter(Collider other) { // Check for when something enters the step stone's collision box.
+        OnStep.Invoke(); // Invoke the event, triggering whatever is linked in the Inspector tab.
+
+        transform.Translate(new Vector3(0, -0.01f, 0));
+    }
+
+    private void OnTriggerExit() {
+        transform.Translate(new Vector3(0, 0.01f, 0));
+    }
+}
--- Assets/Scripts/LightMovementTest.cs
+++ Assets/Scripts/LightMovementTest.cs
@@ -3,10 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class LightMovementTest : MonoBehaviour {
+    private Camera camera;
+
+    void Start() {
+        camera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+    }
+
+    public void Print() {
+        Debug.Log("Stepstone works");
+    }
+
     void Update() {
         float x = Input.GetAxis("Horizontal");
         float y = Input.GetAxis("Vertical");
 
-        transform.Translate(new Vector3(x, 0, y) * Time.deltaTime);
+        RaycastHit hit;
+
+        Ray lookAt = camera.ScreenPointToRay(Input.mousePosition);
+        transform.position += new Vector3(x, 0, y) * Time.deltaTime;
+        
+        if (Physics.Raycast(lookAt, out hit, LayerMask.GetMask("Ground"))) {
+            Vector3 finalPoint = hit.point;
+            finalPoint.y = transform.position.y;
+
+            transform.LookAt(finalPoint);
+        }
     }
 }
