d074db1422f61c4f3e6392bd76fb6cc24dbbc821
Jonas Clermont <clermoj1@gmatc.matc.edu>
2020-02-25 14:08:42 -0600

    Brought in pickup script

---
+++ Assets/Scripts/PickUpPotion.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public enum ItemType { HEALTH, STAMINA, OIL };
+
+public class PickUpPotion : MonoBehaviour {
+    //heals player 25
+    //oil 25
+
+    [SerializeField] private ItemType type;
+
+    [SerializeField] private int potionHealth = 25;
+    [SerializeField] private int potionOil = 25;
+
+    void OnTriggerEnter(Collider other) {
+        //detects pick up
+        if (other.gameObject.CompareTag("Player")) {
+            Inventory pi = other.gameObject.GetComponent<Inventory>();
+            pi.AddPotion((int)type);
+           //destroy the pick up from hierarchy 
+           Destroy(this.gameObject);
+		}
+	}
+}
