a03a536b6cd8f3b6625f470545a4642e8de989ea
Mike Lecus <lecusm@gmatc.matc.edu>
2020-03-02 15:25:46 -0600

    Fix Branch

---
+++ Assets/Scripts/Lantern.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Lantern : MonoBehaviour
+{
+    //public Text oil;
+    public static bool lantern = false;
+    public float currentOilValue = 100f;
+    [SerializeField] public static float currentOil = 100f;
+    [SerializeField] private float maxOil = 100f;
+    [SerializeField] private float minOil = 0f;
+    [SerializeField] private float setOil = 1f;
+    [SerializeField] private int absOil;
+
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (currentOilValue != currentOil) {
+            currentOil = currentOilValue;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Q)) {
+            Debug.Log("Pressed");
+            lantern = !lantern;
+        }
+
+        if (lantern == true && currentOil > 0) {
+            currentOil = Mathf.Max(currentOil - (setOil * Time.deltaTime), 0);
+            //oil.text = "" + Mathf.RoundToInt(currentOil);
+        }
+
+        currentOilValue = currentOil;
+
+        if (currentOil <= 0) {
+            lantern = false;
+        }
+    }
+}
+
---
+++ Assets/Scripts/Sprint.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//Andy Xiong 2/17/20
+public class Sprint : MonoBehaviour
+{
+    [SerializeField] public float maxStamina = 100;
+    [SerializeField] private float currentStamina;
+    [SerializeField] private float regenStamina = 5;
+    //[SerializeField] private int speed = 5;
+    public PlayerController player;
+
+    // Update is called once per frame
+    void Start()
+    {
+        player = GetComponent<PlayerController>();
+        currentStamina = maxStamina;
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            player.speed = 5f;
+            
+        }
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            player.speed = 3f;
+        }
+
+
+
+            if (currentStamina < maxStamina)
+        {
+            Regenerate();
+        }
+    }
+    void Regenerate()
+    {
+        currentStamina = Mathf.Min(currentStamina + regenStamina, maxStamina);
+    }
+}
